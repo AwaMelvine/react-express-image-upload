4de639f560c7e266f4ce75b37f4313853de5387d
Awa Melvine <melvineawa@gmail.com>
2018-07-20 22:27:09 -0700

    Created form. Implemented selecting and previewing image

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: '',
+      profileImage: '',
+      bio: ''
+    };
+  }
+  onChange = e => {
+    if (e.target.type === "file") {
+      // Assuming only image
+      let file = this.refs.profileImage.files[0];
+      let reader = new FileReader();
+      let url = reader.readAsDataURL(file);
+
+      reader.onloadend = e => {
+        this.setState({ imageUrl: [reader.result], profileImage: file });
+      };
+    }
+
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
+  }
+
+  onSubmit = e => {
+
+  }
+
   render() {
+    const { bio, profileImage, imageUrl } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-4">
+
+            <form onSubmit={this.handleSubmit}>
+
+              <div className="profile-div">
+                {
+                  this.state.imageUrl ?
+                  <img src={imageUrl} onClick={() => this.refs.profileImage.click() } className="img-placeholder" alt="" />
+                  :
+                  <div className="img-placeholder" onClick={() => this.refs.profileImage.click() }>
+                    <span className="hidden">Profile Image</span>
+                  </div>
+                }
+                <input type="file" ref="profileImage" onChange={this.onChange} className="hidden" />
+              </div>
+
+              <div className="form-group">
+                <label className="label ">Bio</label>
+                <textarea name="bio" value={bio} className="form-control"></textarea>
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary">Upload user</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
